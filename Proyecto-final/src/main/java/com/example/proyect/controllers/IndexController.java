5cf35eaf3fdb479fad60768e57f1fdaca7cc6adc
Phoslyte <Alicecentral124@outlook.com>
2020-12-08 22:55:12 -0500

    Estas son las bases del proyecto final, tiene tanto el home y la cracion de los htmls para los demas partes.

---
+++ Proyecto-final/src/main/java/com/example/proyect/controllers/IndexController.java
@@ -0,0 +1,31 @@
+package com.example.proyect.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping({"/home","/"})
+	public String index() {
+			 
+		return "index";
+	}
+	
+	@GetMapping({"/acceder","/iniciar"})
+	public String acceder() {
+		return "acceder";
+	}
+	@GetMapping({"/Juegos","/comprar"})
+	public String comprar() {
+		return "comprar";
+	}
+	@GetMapping({"/usuario-nuevo","/registro"})
+	public String registro() {
+		return "registro";
+	}
+	@GetMapping({"/biblio","/pero"})
+	public String biblioteca() {
+		return "biblioteca";
+	}
+}
